b7232edcfc4469216a90222da9068970317f755e
arnovolts <thebluehermit@protonmail.com>
2022-10-23 18:48:21 +0200

    Give classes to form child elements

--- index.php
+++ index.php
@@ -11,28 +11,28 @@
 
     <div class="card-body">
         <form method="POST">
-            <div>
+            <div class="form-control">
                 <input id="fullname" type="text" name="full_name" required>
                 <label for="fullname"> Full Name </label>
             </div>
 
-            <div>
+            <div class="form-control">
                 <input id="email" type="email" name="email" required>
                 <label for="email"> Email </label>
             </div>
 
-            <div>
+            <div class="form-control">
                 <input id="rating" type="number" min="0" max="5" name="rating" required>
                 <label for="rating"> Rating out of five </label>
             </div>
 
-            <div>
+            <div class="form-control">
                 <textarea id="message" name="message">
                 </textarea>
                 <label for="message">Message</label>
             </div>
 
-            <button type="submit">Send</button>
+            <button class="btn" type="submit">Send</button>
         </form>
     </div>
 </article>
