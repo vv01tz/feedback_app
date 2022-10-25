971c27819aeca54e2229b8cdb33e39113647f69f
arnovolts <thebluehermit@protonmail.com>
2022-10-25 10:42:56 +0200

    Add floating label animation

--- index.php
+++ index.php
@@ -12,23 +12,22 @@
     <div class="card-body">
         <form method="POST">
             <div class="form-control">
-                <input id="fullname" type="text" name="full_name" required>
+                <input class="form-input" id="fullname" type="text" name="full_name" required>
                 <label for="fullname"> Full Name </label>
             </div>
 
             <div class="form-control">
-                <input id="email" type="email" name="email" required>
+                <input class="form-input" id="email" type="email" name="email" placeholder="Email" required>
                 <label for="email"> Email </label>
             </div>
 
             <div class="form-control">
-                <input id="rating" type="number" min="0" max="5" name="rating" required>
+                <input class="form-input" id="rating" type="number" min="0" max="5" name="rating" required>
                 <label for="rating"> Rating out of five </label>
             </div>
 
             <div class="form-control">
-                <textarea id="message" name="message">
-                </textarea>
+                <textarea class="form-input" id="message" name="message" required></textarea>
                 <label for="message">Message</label>
             </div>
 
