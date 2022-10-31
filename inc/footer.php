59d8f4e032d26b805603fc23115cd247ce06091a
arnovolts <thebluehermit@protonmail.com>
2022-10-31 17:18:57 +0100

    Link JavaScrip to the html pages

--- inc/footer.php
+++ inc/footer.php
@@ -2,5 +2,7 @@
         <footer class="footer"> 
             Coded by <a href="https://www.github.com/vv01tz" target="_blank">Arno Volts</a>
         </footer>
+        <script src="assets/js/form.js"></script>
+        <script src="assets/js/ratings.js"></script>
     </body>
 </html>
