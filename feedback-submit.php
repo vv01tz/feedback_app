6279e20cccde73bbd61588c091b70cc3503d4a76
arnovolts <thebluehermit@protonmail.com>
2022-11-03 06:17:50 +0100

    Display message with the status of submission

--- feedback-submit.php
+++ feedback-submit.php
@@ -1,23 +1,53 @@
-<?php require 'config.php' ?>
 <?php
-    // Connect to database
-    $db_connection = new mysqli($servername, $db_user, $db_password, $db);
-    if ($db_connection->connect_error)
-    {
-        die("Connection failed".$db_connection->connect_error);
+    function main(){
+        include 'config.php';
+
+        // Connect to the database
+        $db_connection = new mysqli($servername, $db_user, $db_password, $db);
+
+        if ($db_connection->connect_error){
+            $db_connection->close();
+            return false;
+        }
+
+        $result = insert_data($db_connection);
+        $db_connection->close();
+
+        return $result;
     }
 
-    // Insert data into database
-    if (isset($_POST))
-    {
+    function insert_data($db_connection){
+        if (!isset($_POST)){
+            return false;
+        }
+
         $name = $_POST["full_name"];
         $email = $_POST["email"];
         $rating = (int) $_POST["rating"];
         $msg = $_POST["message"];
         $feedback_date = date_format(date_create("UTC"), "Ymd");
         
-        $db_connection->query("INSERT INTO user_feedback(name, email, rating, message, date) VALUES ('$name', '$email', $rating, '$msg', $feedback_date)");
-    }
+        $result = $db_connection->query("INSERT INTO user_feedback(name, email, rating, message, date) VALUES ('$name', '$email', $rating, '$msg', $feedback_date)");
 
-    $db_connection->close();
+        // Returns the result of the query
+        return $result;
+    }
 ?>
+
+<?php require "inc/header.php" ?>
+
+<?php $succes = main() ?>
+
+<div class="status-msg <?php echo $succes ? "succes" : "fail" ?>">
+    <h1>
+        <?php
+            if ($succes){
+                echo "Feedback Submitted Sucessfully";
+            }else {
+                echo "Feedback Submission Failed";
+            }
+        ?>
+    </h1>
+</div>
+
+<?php require "inc/footer.php" ?>
